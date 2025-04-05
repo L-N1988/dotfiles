a38a98aabf5ea2747490a84f2af9560106c0deff
L-N1988 <2994567011@qq.com>
2025-04-05 17:20:15 +0800

    add emacs config from tsoding.

---
+++ tsoding-emacs/.emacs.custom.el
@@ -0,0 +1,29 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(display-line-numbers-type 'relative)
+ '(org-agenda-dim-blocked-tasks nil)
+ '(org-agenda-exporter-settings '((org-agenda-tag-filter-preset (list "+personal"))))
+ '(org-cliplink-transport-implementation 'url-el)
+ '(org-enforce-todo-dependencies nil)
+ '(org-modules
+   '(org-bbdb org-bibtex org-docview org-gnus org-habit org-info org-irc org-mhe org-rmail org-w3m))
+ '(org-refile-use-outline-path 'file)
+ '(package-selected-packages
+   '(evil rainbow-mode proof-general elpy hindent ag qml-mode racket-mode php-mode go-mode kotlin-mode nginx-mode toml-mode love-minor-mode dockerfile-mode nix-mode purescript-mode markdown-mode jinja2-mode nim-mode csharp-mode rust-mode cmake-mode clojure-mode graphviz-dot-mode lua-mode tuareg glsl-mode yaml-mode d-mode scala-mode move-text nasm-mode editorconfig tide company powershell js2-mode yasnippet helm-ls-git helm-git-grep helm-cmd-t helm multiple-cursors magit haskell-mode paredit ido-completing-read+ smex gruber-darker-theme org-cliplink dash-functional dash))
+ '(safe-local-variable-values
+   '((eval progn
+           (auto-revert-mode 1)
+           (rc/autopull-changes)
+           (add-hook 'after-save-hook 'rc/autocommit-changes nil 'make-it-local))))
+ '(warning-minimum-level :error)
+ '(whitespace-style
+   '(face tabs spaces trailing space-before-tab newline indentation empty space-after-tab space-mark tab-mark)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
