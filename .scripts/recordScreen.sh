a1e878c2db8e58320c70cedd42282a4a05daf741
L-N1988 <2994567011@qq.com>
2026-04-21 23:01:07 +0800

    add keybinding for video recording

--- .scripts/recordScreen.sh
+++ .scripts/recordScreen.sh
@@ -7,9 +7,9 @@ IFS=+x read -r w h x y <<EOF
 $selection
 EOF
 
-# Ensure width and height are even (required by most encoders like x264)
-w=$((w + w % 2))
-h=$((h + h % 2))
+# Ensure width and height are even (required by most encoders like x264), round them down to keep it inside the screen boundarie.
+w=$((w - w % 2))
+h=$((h - h % 2))
 
 # Record using ffmpeg
 ffmpeg \
@@ -19,4 +19,5 @@ ffmpeg \
   -i ":0.0+$x,$y" \
   -c:v libx264 -preset ultrafast -crf 0 \
   -pix_fmt yuv420p \
+  -movflags +faststart \
   "recording_$(date +%Y%m%d_%H%M%S).mp4"
